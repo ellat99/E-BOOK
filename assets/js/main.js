512ee1954fe3484a42ef56c7365ebb4b07fc0d5b
tle09 <elena_larisa_t@yahoo.com>
2023-12-04 16:25:57 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,77 @@ let swipertESTIMONIAL = new Swiper(".testimonial__swiper", {
     },
   },
 });
+
+/**================================== SHOW SCROLL UP====================================================== */
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+/**=====================================SCROLL SECTIONS ACTIVE LINK================================ */
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+/*====================DARK LIGHT THEME=================================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+/**============================SCROLL REVEAL ANIMATION================================================= */
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true, //aniamtions repeat
+});
+
+sr.reveal(
+  `.home__data, .featured__container, .new__container, .join__data, .testimonial__container, .footer`
+);
+sr.reveal(`.home__images`, { delay: 600 });
+sr.reveal(`.service__card`, { interval: 100 });
+sr.reveal(`.discount__data`, { origin: `left` });
+sr.reveal(`.discount__images`, { origin: `right` });
